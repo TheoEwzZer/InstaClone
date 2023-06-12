c8bb457c28fce92d4596a7622cccafeaf1063c95
Théo <theo.fabianomattei@gmail.com>
2023-06-12 22:48:07 +0200

    🚀 feat: add main function to run the backend server using uvicorn

---
+++ backend/main.py
@@ -0,0 +1,8 @@
+"""
+This module contains the main function to run the backend server for the instaclone application.
+"""
+
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run(app="app.api:app", host="0.0.0.0", port=8000, reload=True)
