ba1f69026c9015ac09b034d38ac8f505258bcfd9
Théo <theo.fabianomattei@gmail.com>
2023-06-12 22:37:45 +0200

    ✨ feat: add new routes for login and registration pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import Register from "./pages/register";
 
 function App(): React.ReactElement {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/accounts/login/" element={<Home />} />
+        <Route path="/accounts/emailsignup/" element={<Register />} />
       </Routes>
     </Router>
   );
